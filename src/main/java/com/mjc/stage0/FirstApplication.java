86b173596432f97489abdbbd0b4b43336708faae
doniyoryuldashev95 <doniyoryuldashev95@gmail.com>
2022-09-03 23:55:25 +0500

    novini yozolik

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -2,7 +2,7 @@ package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
-        // Write your code here!
+        // Write your code here!4
         System.out.println("Hello, World!");
     }
 }
